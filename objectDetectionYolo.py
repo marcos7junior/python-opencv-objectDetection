1a86e137424726f70e1dd04342954baff9e599a4
marcos7junior <33362130+marcos7junior@users.noreply.github.com>
2021-05-20 07:43:43 -0300

    Add files via upload

---
+++ objectDetectionYolo.py
@@ -0,0 +1,60 @@
+import cv2 as cv
+import numpy as np
+
+path = 'C:\\Users\\marco\\PycharmProjects\\pythonOpenCV\\Resources\\video1.mp4'
+
+cap = cv.VideoCapture(path)
+whT = 320
+confThreshold =0.5
+nmsThreshold= 0.2
+
+classesFile = 'coco.names'
+classNames = []
+with open(classesFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+
+modelConfiguration = 'yolov3.cfg'
+modelWeights = 'yolov3-tiny.weights'
+net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+def findObjects(outputs,img):
+    hT, wT, cT = img.shape
+    bbox = []
+    classIds = []
+    confs = []
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                w,h = int(det[2]*wT) , int(det[3]*hT)
+                x,y = int((det[0]*wT)-w/2) , int((det[1]*hT)-h/2)
+                bbox.append([x,y,w,h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+
+    indices = cv.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
+
+    for i in indices:
+        i = i[0]
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        # print(x,y,w,h)
+        cv.rectangle(img, (x, y), (x+w,y+h), (255, 0 , 255), 2)
+
+
+while True:
+    success, img = cap.read()
+    blob = cv.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+    net.setInput(blob)
+    layersNames = net.getLayerNames()
+    outputNames = [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    outputs = net.forward(outputNames)
+    findObjects(outputs,img)
+
+    cv.imshow('Image', img)
+    cv.waitKey(1)
